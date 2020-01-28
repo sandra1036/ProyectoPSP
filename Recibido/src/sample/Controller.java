4bbfc2b1bab90f74699f62fcccf30f3ae9def30a
sandra <sandraromero262@gmail.com>
2020-01-28 18:21:04 +0100

    Intentar Que vaya la otra ventana y el evento

--- Recibido/src/sample/Controller.java
+++ Recibido/src/sample/Controller.java
@@ -1,19 +1,26 @@
 package sample;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import org.w3c.dom.ls.LSOutput;
 import sample.Objetos.Email;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import sample.Objetos.Email;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Controller implements Initializable {
-
+    Scene scene;
     @FXML
     TableView<Email> tableView;
     @FXML
@@ -22,12 +29,23 @@ public class Controller implements Initializable {
     TableColumn<Email, String> namecolumn;
     @FXML
     TableColumn<Email, String> Emailcolumn;
-    @FXML
-    Button botoncrear= new Button();
 
+    @FXML private Button button;
+
+    Controller cr;
+
+    @FXML private void llamarventana() throws IOException {
+        Stage stage= new Stage();
+        FXMLLoader loader=new FXMLLoader();
+        AnchorPane root=(AnchorPane)loader.load(getClass().getResource("recibido.fxml").openStream());
+        cr=(Controller)loader.getController();
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        botoncrear.setOnMouseClicked(event -> System.out.println("patata"));
-
+        try {
+            llamarventana();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Recibido/src/sample/Main.java
+++ Recibido/src/sample/Main.java
@@ -6,12 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Proyecto");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
